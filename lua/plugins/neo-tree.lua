299a507bc55220673b476c291922ee1376258aa9
Ivan <2001joker2001@gmail.com>
2024-02-06 04:29:14 +0300

    plugins added

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -13,9 +13,9 @@ return {
 			close_if_last_window = true,
 			window = {
 				position = 'right',
-				hijack_netrw_behavior = --"open_default", -- netrw disabled, opening a directory opens neo-tree
+				hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
                                                   -- in whatever position is specified in window.position
-                                "open_current",  -- netrw disabled, opening a directory opens within the
+                                -- "open_current",  -- netrw disabled, opening a directory opens within the
                                                   -- window like netrw would, regardless of window.position
                                 -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
 			}
