7bb521febd65e8672dd42d702d675de868635cdb
Ivan <2001joker2001@gmail.com>
2024-03-19 18:08:13 +0300

    new features added

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -9,16 +9,41 @@ return {
 	},
 
 	config = function()
+		vim.fn.sign_define("DiagnosticSignError",
+			{ text = " ", texthl = "DiagnosticSignError" })
+		vim.fn.sign_define("DiagnosticSignWarn",
+			{ text = " ", texthl = "DiagnosticSignWarn" })
+		vim.fn.sign_define("DiagnosticSignInfo",
+			{ text = " ", texthl = "DiagnosticSignInfo" })
+		vim.fn.sign_define("DiagnosticSignHint",
+			{ text = "󰌵", texthl = "DiagnosticSignHint" })
+
 		require("neo-tree").setup({
 			close_if_last_window = true,
 			window = {
 				position = 'right',
 				hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
-                                                  -- in whatever position is specified in window.position
-                                -- "open_current",  -- netrw disabled, opening a directory opens within the
-                                                  -- window like netrw would, regardless of window.position
-                                -- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
-			}
+				-- in whatever position is specified in window.position
+				-- "open_current",  -- netrw disabled, opening a directory opens within the
+				-- window like netrw would, regardless of window.position
+				-- "disabled",    -- netrw left alone, neo-tree does not handle opening dirs
+			},
+			git_status = {
+				symbols = {
+					-- Change type
+					added     = "✚", -- or "", but this is redundant info if you use git_status_colors on the name
+					modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+					deleted   = "✖", -- this can only be used in the git_status source
+					renamed   = "󰁕", -- this can only be used in the git_status source
+					-- Status type
+					untracked = "",
+					ignored   = "",
+					unstaged  = "󰄱",
+					staged    = "",
+					conflict  = "",
+				}
+			},
+
 		})
 		vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal right<CR>')
 	end
